109127fe1c64cbcc3f21d3704ea9121a366f93ed
Harriet Thompson <46738131+hazthompson@users.noreply.github.com>
2021-06-08 11:10:58 +0100

    Update users routing

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/users', function (req, res) {
+router.get('/', function (req, res) {
   res.send('Got a GET request at /users');
 });
 
-router.post('/user', function (req, res) {
+router.post('/', function (req, res) {
   res.send('Got a POST request at /users');
 });
 
-router.put('/user', function (req, res) {
+router.put('/', function (req, res) {
   res.send('Got a PUT request at /users');
 });
 
-router.delete('/user', function (req, res) {
+router.delete('/', function (req, res) {
   res.send('Got a DELETE request at /users');
 });
 
